c0aa2a7e1196af1039b15409def09e737d264363
Wandersalamander <31392979+Wandersalamander@users.noreply.github.com>
2017-11-28 15:38:53 +0100

    Added safe method to rebase files

--- cst_update_models.py
+++ cst_update_models.py
@@ -2,7 +2,8 @@ import functions as f
 import shutil
 import os
 import datetime
-import cst_model_reader_commented as cmr
+import cst_model_reader as cmr
+import subprocess
 
 
 class model_updater:
@@ -19,17 +20,24 @@ class model_updater:
                 master_8Gaps is then linked with IH1a_8Gaps, IH_1b_8Gaps
                 master_9Gaps is then linked with IH2a_9Gaps, IH_2b_9Gaps
 
-       mute: bool, print infos or mute them '''
+       mute: bool, print infos or mute them
+
+       cst_path: str, speciefies the path of "CST DESIGN ENVIRONMENT.exe"'''
 
     def __init__(self, target_directory, master_directory,
                  keys=[str(i) + "Gaps" for i in range(20)],
-                 mute=True):
+                 mute=True,
+                 cst_path=None):
+        if cst_path:
+            self.cst_path = cst_path
+        else:
+            self.cst_path = "C:/Program Files (x86)/CST STUDIO SUITE 2017/CST DESIGN ENVIRONMENT.exe"
         self.keys = keys
         self.target_directory = target_directory
         assert target_directory[-1] != "/"
         assert "\\" not in target_directory
         self.files = f.get_files(target_directory, ".cst")
-
+        self.mute = mute
         self.master_directory = master_directory
         assert master_directory[-1] != "/"
         assert "\\" not in master_directory
@@ -50,17 +58,23 @@ class model_updater:
         self.__check_for_keys()
         # self.rebase()
 
-    def rebase(self, skipParams=[]):
+    # def rebase(self, skipParams=[]):
+    def rebase(self, method="slow"):
         '''updates all cavitys using the new master file
 
             skipParams: list, ignores the given parameter names
                         when creating the rebased files
+
+            method: str,"slow" or "scary",
+                       "slow" uses cst import parameter method, safe to use
+                       "scary" replaces parameter values in Model.par,
+                                        its very fast but may lead to errors
         '''
-        self.skipParams = [x.lower() for x in skipParams]
-        assert isinstance(skipParams, list)
+        # self.skipParams = [x.lower() for x in skipParams]
+        # assert isinstance(skipParams, list)
         self.__move_to_subfolder()
         self.__copy_from_master()
-        self.__update_parameters()
+        self.__update_parameters(method=method)
 
     def __check_for_keys(self):
         '''checks if all required files are given by the master files
@@ -107,27 +121,52 @@ class model_updater:
             shutil.copyfile(src, to_file)
             shutil.copytree(src.split(".")[0], to_folder)
 
-    def __update_parameters(self):
+    def __update_parameters(self, method="slow"):
         '''updates the parameters in the newly copied
            (copied from master to target directory)
-           files'''
-        if self.mute is False:
-            print('''\n\nReading parameters from outdated file 
-            	and writing it to renamed master
-            	files to target_directory\n\n''')
-        for to_file in self.files:
+           files
+
+           method: str,"slow" or "scary",
+                       "slow" uses cst import parameter method, safe to use
+                       "scary" replaces parameter values in Model.par,
+                                        its very fast but may lead to errors
+              '''
+
+        def scary(self, from_file, to_file):
             to_cst = cmr.CST_Model(to_file)
-            from_cst = cmr.CST_Model(self.subfolder + to_file.split("/")[-1])
+            from_cst = cmr.CST_Model(from_file)
             for param in from_cst.getParams():
-                # print(param)
                 if param[0].lower() not in self.skipParams:
                     to_cst.editParam(param[0], param[1])
+
+        def slow(self, from_file, to_file):
+            par_path = "Model/3D/Model.par"
+            from_par = from_file.split(".")[0] + par_path
+            flag = " -c -par " + from_par
+            cmd = self.cst_path + flag + " " + to_file
+            assert os.path.isfile(from_file)
+            assert os.path.isfile(to_file)
+            assert os.path.isfile(from_par)
+            subprocess.call(cmd)
+        methods = ["slow", "scary"]
+        assert method in methods
+        if self.mute is False:
+            print('''\n\nReading parameters from outdated file 
+                and writing it to renamed master
+                files to target_directory\n\n''')
+        for to_file in self.files:
+            from_file = self.subfolder + to_file.split("/")[-1]
+            print(to_file)
+            if method == "scary":
+                scary(self, from_file, to_file)
+            elif method == "slow":
+                slow(self, from_file, to_file)
 
 
 if __name__ == "__main__":
     def test0():
         u = model_updater("C:/Users/Simon/Desktop/IHs_newDD_notBase",
-                             "C:/Users/Simon/Desktop/IHs_newDD/Tuner and Frequ")
+                          "C:/Users/Simon/Desktop/IHs_newDD/Tuner and Frequ")
 
     def test01():
         subfolder = "C:/Dropbox/Uni_privat/Master/Python/CST/TEST/OLD/201710181453/"
