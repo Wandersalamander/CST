f6a94340f411045f065d197a4bb2271334514d2b
Wandersalamander <31392979+Wandersalamander@users.noreply.github.com>
2018-03-20 11:20:43 +0100

    Docstrings

--- cst_optimizer_draft.py
+++ cst_optimizer_draft.py
@@ -11,8 +11,16 @@ import copy
 
 
 class plotting():
-
     def __init__(self, param_labels):
+        '''Starts interactive plotting
+
+
+        Parameters
+        ----------
+        param_labels : list
+            y_labels to be plotted in subfigures
+            (use draw method to plot to this labels)
+            '''
         plt.figure(0)
         plt.ion()
         self.x = 0
@@ -26,6 +34,16 @@ class plotting():
         plt.show()
 
     def draw(self, cost, params):
+        '''Draws cost and params into subplots
+
+
+        Parameters
+        ----------
+        cost : float
+            cost function value to be plotted
+        params : list of floats
+            ordered as param_labels to be plotted
+        '''
         plt.figure(0)
         assert len(params + [cost]) == len(self.labels)
         for i, y in enumerate([cost] + params):
@@ -37,10 +55,19 @@ class plotting():
 
 
 class optimizer():
+    '''Interface to controll optimization of CST Model
+
+
+    Parameters
+    ----------
+    path : str
+        path to cst model which is subject
+        to optimization
+    '''
+
     def __init__(self, path):
-        if path[-3:] != "cst":
+        if path[-4:] != ".cst":
             raise Exception("path does not end on .cst")
-        # assert path[-3:] == "cst"
         self.path = path.replace("\\", "/")
         self.variables = []  # list of strings
         self.goals = goals()
@@ -50,6 +77,20 @@ class optimizer():
         self.dc = None  # distributet computing
 
     def set_variable(self, var):
+        '''Selects CST-parameter as variable for optimization
+
+
+        Note
+        ----
+        This method does not handle wether
+        a cst-parameter is existing or not
+
+        Parameters
+        ----------+
+        var : str or list of str
+            name of cst parameter
+
+        '''
         if isinstance(var, str):
             self.variables.append(var)
         elif isinstance(var, list):
@@ -61,6 +102,33 @@ class optimizer():
         self.variables_set = True
 
     def set_goal(self, name, operator, target, weight=1.0):
+        '''Sets goal for optimization
+
+
+        To set targets like
+        f0 = 10Hz
+        U0 > 5kV
+
+
+        Note
+        ----
+        This method does not handle wether
+        a cst-result is existing or not
+
+
+        Parameters
+        ----------
+        name : str
+            Name of result, located at
+            filepath/results/name.rd0
+        operator : str
+            ">", "<" or "="
+        target : float
+            target to optimize goal
+        weight : float, optional
+            weight
+
+        '''
         self.goals.add(
             name=name,
             operator=operator,
@@ -70,7 +138,23 @@ class optimizer():
         self.goals_set = True
         pass
 
-    def start_minimizer(self):
+    def start_minimizer(self, method="Newton-CG"):
+        '''Starts minimizer
+
+
+        Note
+        ----
+        Jacoby-matrix buggy
+
+
+        Parameters
+        ----------
+        method : str
+            scipy.optimize.minimize method.
+            Select 'Nelder-Mead'
+            or a method which uses jacobian
+
+        '''
         if not self.goals_set:
             raise Exception("use set_goal first")
         if not self.variables_set:
@@ -82,29 +166,64 @@ class optimizer():
             x0.append(triple[2])
         # start minimization
         self.view = plotting(self.variables)
-        res = minimize(
-            fun=self.cost_jac,
-            x0=x0,
-            method="Newton-CG",
-            jac=True,
-            options={'maxiter': 10},
-        )
+        if method == "Nelder-Mead":
+            res = minimize(
+                fun=self.__cost,
+                x0=x0,
+                method=method,
+                options={'maxiter': 10},
+            )
+        else:
+            res = minimize(
+                fun=self.cost_jac,
+                x0=x0,
+                method=method,
+                jac=True,
+                options={'maxiter': 10},
+            )
         print(res.message)
         print()
         print("recalculating with best result")
         self.__cost(res.x)
 
     def set_solver_server(self, ip, port):
-        '''use this to activate distributed computing'''
+        '''Use this to activate distributed computing
+
+
+        Parameters
+        ----------
+        ip : str or int
+            ip of dc solver server
+        port : str or int
+            port of dc solver server
+
+        '''
         self.dc = str(ip) + ":" + str(port)
 
     def cost_jac(self, x, delta=1e-6):
-        ''' x: input vectior
-            delta: float, difference to compute
-                   jacoby matrix numerically
+        '''Compute jacobi-matrix numerically
 
-            return:s float, numpy array, this is
-                     the cost funtion and the jacoby martix'''
+        Note
+        ----
+        Currently not debugged
+
+
+        Parameters
+        ----------
+        x : input vector
+            from here deviation will be computed
+        delta : float,
+            difference to compute
+            jacoby matrix numerically
+
+        Returns
+        -------
+        float
+            this is the cost funtion
+        numpy array
+            the jacoby martix
+
+        '''
         jac = []
         cost = self.__cost(x)
         self.view.draw(cost, x)
@@ -119,11 +238,21 @@ class optimizer():
         return cost, np.array(jac)
 
     def __cost(self, x):
-        ''' x: vector
+        '''Returns cost function
 
-            returns float, cost-value'''
+
+        Parameters
+        ----------
+        x: vector
+
+
+        Returns
+        -------
+        float
+            cost-value'''
 
         def generate_results(self):
+            '''Generates results by runnung eigenmode solver'''
             # x[i] corresponds to self.variables[i]
             # here x variables are applied to the cst model
             for i, Paramname in enumerate(self.variables):
@@ -136,7 +265,8 @@ class optimizer():
             # reinitiallize to obtain current parameter and results
 
         def cost(self):
-            self.cst_model = cmr.CST_Model(path)
+            '''Computes cost with all Goals'''
+            self.cst_model = cmr.CST_Model(self.path)
             # computing the cost function
             cost = 0
             for goal in self.goals.get():
@@ -171,6 +301,7 @@ class optimizer():
 
 
 class goals():
+    '''Class to manage all goals'''
     memory = []
     operators = [">", "<", "="]
 
@@ -188,7 +319,7 @@ class goals():
 class tests():
 
     def opt_f0():
-        path = "C:/Users/Simon/Desktop/PyOpt/IH_8a_7Gaps.cst"
+        path = "C:/Users/Simon/Desktop/Optimizer-test/optf0test.cst"
         opt = optimizer(path)
         opt.set_variable([
             "sp_Shell_width",
@@ -197,5 +328,8 @@ class tests():
 
         opt.set_goal(name="Frequency (Mode 1)", operator="=", target=108.408)
 
-        opt.set_solver_server("141.2.245.144", "36000")
+        opt.set_solver_server("141.2.245.144", "36100")
         opt.start_minimizer()
+
+if __name__ == "__main__":
+    tests.opt_f0()
